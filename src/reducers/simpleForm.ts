858b530d6f3812025071e5392d2e5fdcd915f1d6
yanlele <331393627@qq.com>
2019-03-02 21:53:02 +0800

    refactor(refactor): refactor code

--- src/reducers/simpleForm.ts
+++ src/reducers/simpleForm.ts
@@ -2,13 +2,13 @@ const STATE = {
   firstName: 'name',
   lastName: 'lele',
   email: 'bilibli@qq.com',
-}
+};
 export default function simpleForm(state = STATE, action: {type: string, payload: object}) {
   switch (action.type) {
     case 'SUBMIT':
-      return Object.assign({}, state, action.payload)
+      return Object.assign({}, state, action.payload);
     case 'RESET':
-      return Object.assign({}, {}, state)
+      return Object.assign({}, {}, state);
     default:
       return state
   }
