a26f7bb4e1d9c75344d9728019b1fce2427fa1c7
yanlele <331393627@qq.com>
2019-03-13 18:27:24 +0800

    feat(router): change router to config

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,24 +5,27 @@ import {
   BrowserRouter as Router,
   // HashRouter as Router,
 } from 'react-router-dom';
-import { LocaleProvider } from 'antd';
+import {LocaleProvider} from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-import loadableComponent from './loadableComponent';
-
-const CounterContainer = () => import('containers/CounterContainer');
-const HomeContainer = () => import('containers/HomeContainer');
-const NoMatchContainer = () => import('containers/NoMatchContainer');
+import pageRouteConfig from './pageRouteConfig';
 
 const routes = (
-  <Router>
-    <LocaleProvider locale={zhCN}>
-      <Switch>
-        <Route exact path="/" component={loadableComponent(HomeContainer)} />
-        <Route path='/counter' component={loadableComponent(CounterContainer)} />
-        <Route component={loadableComponent(NoMatchContainer)} />
-      </Switch>
-    </LocaleProvider>
-  </Router>
+    <Router>
+      <LocaleProvider locale={zhCN}>
+        <Switch>
+          {pageRouteConfig.map((route, index) => {
+            const {path, exact, component, title} = route;
+            return <Route
+                key={index}
+                exact={exact}
+                path={path}
+                component={component}
+                title={title}
+            />;
+          })}
+        </Switch>
+      </LocaleProvider>
+    </Router>
 );
 
 export default routes;
